7700acc98cd5d0d78fc0ca660369ecdf28fce225
Thibyaa Mahasivam <thibyaa@gmail.com>
2023-10-16 14:01:55 +0100

    Fruit class completed

--- src/main/java/Fruit.java
+++ src/main/java/Fruit.java
@@ -1,2 +1,39 @@
 public class Fruit {
+
+    private String name;
+    private double price;
+    private boolean expired;
+
+//    CONSTRUCTOR
+    public Fruit (String name, double price, boolean expired){
+        this.name = name;
+        this.price = price;
+        this.expired = expired;
+    }
+
+//    GETTERS AND SETTERS
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isExpired() {
+        return this.expired;
+    }
+
+    public void setExpired(boolean expired) {
+        this.expired = expired;
+    }
 }
