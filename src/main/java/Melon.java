4332a557b1eed99119b3b3e6f08134ff16ccc7d8
Thibyaa Mahasivam <thibyaa@gmail.com>
2023-10-16 14:34:17 +0100

    melon class completed

--- src/main/java/Melon.java
+++ src/main/java/Melon.java
@@ -1,2 +1,17 @@
-public class Melon {
+public class Melon extends Fruit{
+
+    private boolean preCut;
+
+    public Melon(String name, double price, boolean expired, boolean preCut){
+        super(name, price, expired);
+        this.preCut = preCut;
+    }
+
+    public boolean isPreCut() {
+        return preCut;
+    }
+
+    public void setPreCut(boolean preCut) {
+        this.preCut = preCut;
+    }
 }
