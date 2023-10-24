616b435973ab2d0e3caa6f5de34b20f8ba25b3f9
Thibyaa Mahasivam <thibyaa@gmail.com>
2023-10-24 20:12:46 +0100

    fruit shop tests completed

--- src/main/java/FruitShop.java
+++ src/main/java/FruitShop.java
@@ -5,4 +5,72 @@ public class FruitShop {
     private String name;
     private double till;
     private ArrayList<Fruit> fruitStock;
+    private ArrayList<Customer> customers;
+
+    public FruitShop(String name, double till){
+        this.name = name;
+        this.till = till;
+        this.fruitStock = new ArrayList<>();
+        this.customers = new ArrayList<>();
+    }
+//    CUSTOM METHODS
+    public void addFruitToStock(Fruit fruit){
+        this.fruitStock.add(fruit);
+    }
+
+    public int countStock(){
+        return this.fruitStock.size();
+    }
+
+  public void removeFruitFromStock(Fruit fruit){
+        this.fruitStock.remove(fruit);
+  }
+    public void sellFruits(Customer customer){
+        customer.getBasket().calculateTotal();
+        double basketTotal = customer.getBasket().getTotal();
+
+        if(customer.getWallet() > basketTotal){
+            for(Fruit fruit: customer.getBasket().getFruits()){
+                removeFruitFromStock(fruit);
+            }
+            setTill(this.till + basketTotal);
+        } else System.out.println("You do not have enough money");
+    }
+
+    public void addCustomerToShop(Customer customer){
+        this.customers.add(customer);
+    }
+
+//    GETTERS AND SETTERS
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getTill() {
+        return till;
+    }
+
+    public void setTill(double till) {
+        this.till = till;
+    }
+
+    public ArrayList<Fruit> getFruitStock() {
+        return fruitStock;
+    }
+
+    public void setFruitStock(ArrayList<Fruit> fruitStock) {
+        this.fruitStock = fruitStock;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(ArrayList<Customer> customers) {
+        this.customers = customers;
+    }
 }
--- src/test/java/FruitShopTest.java
+++ src/test/java/FruitShopTest.java
@@ -1,2 +1,98 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class FruitShopTest {
+
+    FruitShop kavsFruit;
+    Fruit watermelon;
+    Fruit orange;
+    Customer newCustomer;
+
+    @BeforeEach
+    void setUp(){
+        kavsFruit = new FruitShop("Kav's Fruit", 100.00);
+        watermelon = new Melon("watermelon", 6.00, LocalDate.of(2023, 10, 20), 5, true);
+        orange = new Citrus("Orange", 8.99, LocalDate.of(2023, 10, 18), 5, true);
+        kavsFruit.addFruitToStock(watermelon);
+        newCustomer  = new Customer("Loshanth", 789, true);
+        kavsFruit.addCustomerToShop(newCustomer);
+        newCustomer.getBasket().addFruitToBasket(orange);
+
+    }
+
+    @Test
+    void canGetName(){
+        assertThat(kavsFruit.getName()).isEqualTo("Kav's Fruit");
+    }
+
+    @Test
+    void canSetName(){
+        kavsFruit.setName("G's Fruit");
+        assertThat(kavsFruit.getName()).isEqualTo("G's Fruit");
+    }
+
+    @Test
+    void canGetTill(){
+        assertThat(kavsFruit.getTill()).isEqualTo(100.00);
+    }
+
+    @Test
+    void canSetTill(){
+        kavsFruit.setTill(0.00);
+        assertThat(kavsFruit.getTill()).isEqualTo(0.00);
+    }
+
+    @Test
+    void canGetFruitStock(){
+        assertThat(kavsFruit.getFruitStock().size()).isEqualTo(1);
+    }
+
+    @Test
+    void canSetFruitStock(){
+        ArrayList<Fruit> freshStock = new ArrayList<>();
+        kavsFruit.setFruitStock(freshStock);
+        assertThat(kavsFruit.getFruitStock().size()).isEqualTo(0);
+    }
+
+    @Test
+    void canGetCustomers(){
+        assertThat(kavsFruit.getCustomers().size()).isEqualTo(1);
+    }
+
+    @Test
+    void canSetCustomers(){
+        ArrayList<Customer> noCustomers = new ArrayList<>();
+        kavsFruit.setCustomers(noCustomers);
+        assertThat(kavsFruit.getCustomers().size()).isEqualTo(0);
+    }
+
+    @Test
+    void canAddFruitsToStock(){
+        kavsFruit.addFruitToStock(orange);
+        assertThat(kavsFruit.getFruitStock().size()).isEqualTo(2);
+    }
+
+    @Test
+    void canCountStock(){
+        assertThat(kavsFruit.countStock()).isEqualTo(1);
+    }
+
+    @Test
+    void canRemovefruit(){
+        kavsFruit.removeFruitFromStock(watermelon);
+        assertThat(kavsFruit.getFruitStock().size()).isEqualTo(0);
+    }
+
+    @Test
+    void canSellFruit_customerHasMoney(){
+        kavsFruit.sellFruits(newCustomer);
+        assertThat(kavsFruit.getFruitStock().contains(orange)).isEqualTo(false);
+        assertThat(kavsFruit.getTill()).isEqualTo(108.99);
+    }
 }
+
